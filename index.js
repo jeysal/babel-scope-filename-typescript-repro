019207a1e114d6c3229f894147b950d921914f99
Tim Seckinger <seckinger.tim@gmail.com>
2018-03-18 21:22:10 +0100

    add repro

---
+++ index.js
@@ -0,0 +1,34 @@
+const { transform } = require("@babel/core");
+
+const plugin = ({ types: t }) => ({
+  visitor: {
+    Program(path) {
+      const [importPath] = path.unshiftContainer(
+        "body",
+        t.importDeclaration(
+          [t.importDefaultSpecifier(t.identifier("local"))],
+          t.stringLiteral("source")
+        )
+      );
+      path.scope.registerDeclaration(importPath);
+      //path.scope.crawl() // same result
+    }
+  }
+});
+
+console.log(
+  transform(`a();`, {
+    presets: ["@babel/preset-env"],
+    plugins: [plugin],
+    filename: "test.js"
+  }).code
+);
+
+/*
+  Generated code misses the import.
+  Ways to fix this (ONE of the following is sufficient):
+  * remove the `filename` in the transform call
+  * change `a()` to `local()`
+  * disable `@babel/preset-typescript` in .babelrc
+  * do not `registerDeclaration` / `crawl`
+*/
