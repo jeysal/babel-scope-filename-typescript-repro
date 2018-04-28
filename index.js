f4c398ae3b60786119c6e95e6509b0bc4d3bcb1d
Tim Seckinger <seckinger.tim@gmail.com>
2018-04-28 16:59:50 +0200

    simplify

--- index.js
+++ index.js
@@ -2,33 +2,35 @@ const { transform } = require("@babel/core");
 
 const plugin = ({ types: t }) => ({
   visitor: {
-    Program(path) {
-      const [importPath] = path.unshiftContainer(
-        "body",
-        t.importDeclaration(
-          [t.importDefaultSpecifier(t.identifier("local"))],
-          t.stringLiteral("source")
-        )
-      );
-      path.scope.registerDeclaration(importPath);
-      //path.scope.crawl() // same result
+    CallExpression(path) {
+      if (path.node.callee.name === "a") {
+        path.scope
+          .getProgramParent()
+          .path.unshiftContainer(
+            "body",
+            t.importDeclaration(
+              [t.importDefaultSpecifier(t.identifier("local"))],
+              t.stringLiteral("source")
+            )
+          );
+
+        path.scope.crawl();
+        path.node.callee = t.identifier("local");
+      }
     }
   }
 });
 
 console.log(
   transform(`a();`, {
-    presets: ["@babel/preset-env"],
-    plugins: [plugin],
-    filename: "test.js"
+    presets: ["@babel/preset-typescript"],
+    plugins: [plugin]
   }).code
 );
 
 /*
   Generated code misses the import.
   Ways to fix this (ONE of the following is sufficient):
-  * remove the `filename` in the transform call
-  * change `a()` to `local()` - NEVERMIND THIS DOES NOT FIX IT
-  * disable `@babel/preset-typescript` in .babelrc
-  * do not `registerDeclaration` / `crawl`
+  * disable `@babel/preset-typescript`
+  * `crawl` one line later
 */
